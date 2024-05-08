e86f061432fcea8dbe25ddcd0cdc2f82de7a76a5
Adesh Kanthali <92244882+Adeshkanthali@users.noreply.github.com>
2024-05-08 23:14:02 +0530

    Add files via upload

---
+++ Email Sender Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Toaster } from 'react-hot-toast'
+import './App.css'
+import EmailSender from './components/EmailSender'
+
+function App() {
+
+  return (
+    <>
+    <EmailSender/>
+    <Toaster/>
+    </>
+  )
+}
+
+export default App
